73ee941a578c8273e23cc1de891560bcde8d51f4
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-04 13:09:05 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,192 @@
+var config = {
+    style: 'mapbox://styles/dogatmn/cm4v9fem6000p01sf12f769y5/draft',
+    accessToken: 'pk.eyJ1IjoiZG9nYXRtbiIsImEiOiJjbHlyMTJmcXgwMjQ3MmtzbTRsdm1pejdsIn0.vcEjL7lt8OoOPMP5o1PM6g',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    inset: true,
+    insetOptions: {
+        markerColor: 'orange'
+    },
+    insetPosition: 'bottom-right',
+    theme: 'light',
+    use3dTerrain: false,
+    auto: false,
+    title: 'Waste and Sanitation',
+    subtitle: '',
+    byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
+    chapters: [
+        {
+            id: '1',
+            alignment: 'centered',
+            hidden: false,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.966 , 40.999],
+                zoom: 10,
+                pitch: 40,
+                bearing: 20
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+           onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+           {
+            id: '2',
+            alignment: 'centered',
+            hidden: false,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.967, 41.000],
+                zoom: 12,
+                pitch: 40,
+                bearing: 20
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+           onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+           {
+            id: '3',
+            alignment: 'centered',
+            hidden: false,
+            title: '',
+            image: 'https://i.ibb.co/DHZ53gkK/MEF-AAP-027-BAP-2025-06-20-08-41-33.png',
+            description: '',
+            location: {
+                center: [28.964 , 41.002],
+                zoom: 13,
+                pitch: 40,
+                bearing: 6
+                
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+      
+          {
+            id: '4',
+            alignment: 'centered',
+            hidden: true,
+            title: '',
+            image: 'https://i.ibb.co/PsHJYLj4/MEF-AAP-027-BAP-2025-06-20-08-42-23.png',
+            description: '',
+            location: {
+                center: [28.976 , 40.980],
+                zoom: 15,
+                pitch: 40,
+                bearing: 6
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+           onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+           {
+            id: '5',
+            alignment: 'centered',
+            hidden: true,
+            title: '',
+            image: 'https://i.ibb.co/PsHJYLj4/MEF-AAP-027-BAP-2025-06-20-08-42-23.png',
+            description: '',
+            location: {
+                center: [28.990 , 40.921],
+                zoom: 16,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+          onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+           {
+            id: '6',
+            alignment: 'centered',
+            hidden: false,
+            title: '',
+            image: 'https://i.ibb.co/PsHJYLj4/MEF-AAP-027-BAP-2025-06-20-08-42-23.png',
+            description: '',
+            location: {
+                center: [28.973 , 40.876],
+                zoom: 16,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        
+    ]
+};
