ff9775905bc1e547260c716400a7f509fffa6d7a
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-30 11:37:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false,
     auto: false,
-    title: 'Atık ve Sanitasyon / Waste and Sanitation',
+    title: 'Köpekler / Stray Dogs',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
